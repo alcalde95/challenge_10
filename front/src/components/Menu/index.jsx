a3dff5feb1a31800805a4693a5b98e03cc3637ba
Jorge Alcalde <alcalde.ap95@gmail.com>
2023-09-28 19:53:14 +0200

    comprobaciones terminadas

--- front/src/components/Menu/index.jsx
+++ front/src/components/Menu/index.jsx
@@ -4,7 +4,7 @@ import { MyEstadoGlobalContext } from '../../Context/MyEstadoGlobalContext'
 import logo from './logo-sinfondo.png'
 import { useNavigate } from 'react-router-dom'
 import { useEffect } from 'react'
-import { comprobarEmail, comprobarPassword , mostrar , ocultar} from '../comprobaciones.jsx'
+import { comprobarEmail, comprobarPassword, mostrar, ocultar } from '../comprobaciones.jsx'
 
 const Menu = () => {
 
@@ -31,9 +31,36 @@ const Menu = () => {
     }
 
     const comprobarNombre = () => {
-         nombre_caja.length < 3 || nombre_caja.length > 20 ?  mostrar('errorNombreCaja','El tamaño del nombre debe estar entre 3 y 20 caracteres') : ocultar('errorNombreCaja')
-         return 3 >= nombre_caja.length >=20
+        nombre_caja.length < 3 || nombre_caja.length > 20 ? mostrar('errorNombreCaja', 'El tamaño del nombre debe estar entre 3 y 20 caracteres') : ocultar('errorNombreCaja')
+        return 3 >= nombre_caja.length >= 20
     }
+
+    const comprobardescription_caja = () => {
+        description_caja.length < 3 || description_caja.length > 20 ? mostrar('errordescription', 'El tamaño de la descripción debe estar entre 25 y 200 caracteres') : ocultar('errordescription')
+        return 25 >= description_caja.length >= 200
+    }
+
+    const comprobarsoil_caja = () => {
+        soil_caja.length < 3 || soil_caja.length > 20 ? mostrar('errorsoil', 'El tamaño del soil debe estar entre 3 y 20 caracteres') : ocultar('errorsoil')
+        return 3 >= soil_caja.length >= 20
+    }
+    const comprobartemperature_caja = () => {
+        temperature_caja.length < 3 || temperature_caja.length > 20 ? mostrar('errortemperature', 'El tamaño de la temperature debe estar entre 3 y 20 caracteres') : ocultar('errortemperature')
+        return 3 >= temperature_caja.length >= 20
+    }
+    const comprobarsun_caja = () => {
+        sun_caja.length < 3 || sun_caja.length > 20 ? mostrar('errorsun', 'El tamaño del sun debe estar entre 3 y 20 caracteres') : ocultar('errorsun')
+        return 3 >= sun_caja.length >= 20
+    }
+    const comprobarwater_caja = () => {
+        water_caja.length < 3 || water_caja.length > 20 ? mostrar('errorwater', 'El tamaño del water debe estar entre 3 y 20 caracteres') : ocultar('errorwater')
+        return 3 >= water_caja.length >= 20
+    }
+    const comprobarmultiplication_caja = () => {
+        multiplication_caja.length < 3 || multiplication_caja.length > 20 ? mostrar('errormultiplication', 'El tamaño del multiplication debe estar entre 3 y 20 caracteres') : ocultar('errormultiplication')
+        return 3 >= multiplication_caja.length >= 20
+    }
+   
 
     return (
         <div className='flex flex-col justify-center items-center h-[100vh] bg-gray-500 text-neutral-50'>
@@ -44,6 +71,153 @@ const Menu = () => {
             >
                 Añadir
             </button>
+
+
+            <form>
+                <input
+                    type="text"
+                    id='caja_nombre'
+                    className='bg-neutral-500 p-2 rounded-lg w-full text-center placeholder:text-neutral-50 text-neutral-50 outline-none'
+                    value={nombre_caja}
+                    style={{ display: 'none' }}
+                    placeholder='Nombre'
+                    name='caja_nombre'
+                    autoComplete='off'
+                    onChange={e => setnombre_caja(e.target.value)}
+                    onBlur={() => comprobarNombre()}
+                >
+                </input>
+
+                <p id='errorNombreCaja' style={{ display: 'none' }} className='text-red-700 bg-zinc-300	border-2 rounded-md'></p>
+
+                <input
+                    type="text"
+                    id='species_caja'
+                    className='bg-neutral-500 p-2 rounded-lg w-full text-center placeholder:text-neutral-50 text-neutral-50 outline-none'
+                    value={species_caja}
+                    style={{ display: 'none' }}
+                    placeholder='species'
+                    name='species_caja'
+                    autoComplete='off'
+                    onChange={e => setspecies_caja(e.target.value)}
+                    onBlur={() => comprobarspecies_caja()}
+                >
+                </input>
+
+                <p id='errorspecies' style={{ display: 'none' }} className='text-red-700 bg-zinc-300	border-2 rounded-md'></p>
+
+                <input
+                    type="text"
+                    id='description_caja'
+                    className='bg-neutral-500 p-2 rounded-lg w-full text-center placeholder:text-neutral-50 text-neutral-50 outline-none'
+                    value={description_caja}
+                    style={{ display: 'none' }}
+                    placeholder='description'
+                    name='description_caja'
+                    autoComplete='off'
+                    onChange={e => setdescription_caja(e.target.value)}
+                    onBlur={() => comprobardescription_caja()}
+                >
+                </input>
+
+                <p id='errordescription' style={{ display: 'none' }} className='text-red-700 bg-zinc-300	border-2 rounded-md'></p>
+
+                <input
+                    type="text"
+                    id='soil_caja'
+                    className='bg-neutral-500 p-2 rounded-lg w-full text-center placeholder:text-neutral-50 text-neutral-50 outline-none'
+                    value={soil_caja}
+                    style={{ display: 'none' }}
+                    placeholder='Soil'
+                    name='soil_caja'
+                    autoComplete='off'
+                    onChange={e => setsoil_caja(e.target.value)}
+                    onBlur={() => comprobarsoil_caja()}
+                >
+                </input>
+
+                <p id='errorsoil' style={{ display: 'none' }} className='text-red-700 bg-zinc-300	border-2 rounded-md'></p>
+
+                <input
+                    type="text"
+                    id='temperature_caja'
+                    className='bg-neutral-500 p-2 rounded-lg w-full text-center placeholder:text-neutral-50 text-neutral-50 outline-none'
+                    value={temperature_caja}
+                    style={{ display: 'none' }}
+                    placeholder='Temperature'
+                    name='temperature_caja'
+                    autoComplete='off'
+                    onChange={e => settemperature_caja(e.target.value)}
+                    onBlur={() => comprobartemperature_caja()}
+                >
+                </input>
+
+                <p id='errortemperature' style={{ display: 'none' }} className='text-red-700 bg-zinc-300	border-2 rounded-md'></p>
+
+                <input
+                    type="text"
+                    id='sun_caja'
+                    className='bg-neutral-500 p-2 rounded-lg w-full text-center placeholder:text-neutral-50 text-neutral-50 outline-none'
+                    value={sun_caja}
+                    style={{ display: 'none' }}
+                    placeholder='Sun'
+                    name='sun_caja'
+                    autoComplete='off'
+                    onChange={e => setsun_caja(e.target.value)}
+                    onBlur={() => comprobarsun_caja()}
+                >
+                </input>
+
+                <p id='errorsun' style={{ display: 'none' }} className='text-red-700 bg-zinc-300	border-2 rounded-md'></p>
+
+                <input
+                    type="text"
+                    id='water_caja'
+                    className='bg-neutral-500 p-2 rounded-lg w-full text-center placeholder:text-neutral-50 text-neutral-50 outline-none'
+                    value={water_caja}
+                    style={{ display: 'none' }}
+                    placeholder='Water'
+                    name='water_caja'
+                    autoComplete='off'
+                    onChange={e => setwater_caja(e.target.value)}
+                    onBlur={() => comprobarwater_caja()}
+                >
+                </input>
+
+                <p id='errorwater' style={{ display: 'none' }} className='text-red-700 bg-zinc-300	border-2 rounded-md'></p>
+
+                <input
+                    type="text"
+                    id='multiplication_caja'
+                    className='bg-neutral-500 p-2 rounded-lg w-full text-center placeholder:text-neutral-50 text-neutral-50 outline-none'
+                    value={multiplication_caja}
+                    style={{ display: 'none' }}
+                    placeholder='Multiplication'
+                    name='multiplication_caja'
+                    autoComplete='off'
+                    onChange={e => setmultiplication_caja(e.target.value)}
+                    onBlur={() => comprobarmultiplication_caja()}
+                >
+                </input>
+
+                <p id='errormultiplication' style={{ display: 'none' }} className='text-red-700 bg-zinc-300	border-2 rounded-md'></p>
+
+                <input
+                    type="file"
+                    id='foto'
+                    className='bg-neutral-500 p-2 rounded-lg w-full text-center placeholder:text-neutral-50 text-neutral-50 outline-none'
+                    accept="image/*"
+                    style={{ display: 'none' }}
+                    placeholder='foto'
+                    name='foto'
+                    autoComplete='off'
+
+                >
+                </input>
+
+                <p id='errorNombreNuevo' style={{ display: 'none' }} className='text-red-700 bg-zinc-300	border-2 rounded-md'></p>
+
+            </form>
 
             <table className="table-auto border-separate border-spacing-x-2.5 border-spacing-y-2.5 border border-slate-500 bg-amber-950">
 
@@ -101,143 +275,6 @@ const Menu = () => {
                 </tbody>
             </table>
 
-            <form>
-                <input
-                    type="text"
-                    id='caja_nombre'
-                    className='bg-neutral-500 p-2 rounded-lg w-full text-center placeholder:text-neutral-50 text-neutral-50 outline-none'
-                    value={nombre_caja}
-                    style={{ display: 'none' }}
-                    placeholder='Nombre'
-                    name='caja_nombre'
-                    autoComplete='off'
-                    onChange={e => setnombre_caja(e.target.value)}
-                    onBlur={() => comprobarNombre()}
-                >
-                </input>
-
-                <p id='errorNombreCaja' style={{ display: 'none' }} className='text-red-700 bg-zinc-300	border-2 rounded-md'></p>
-
-                <input
-                    type="text"
-                    id='species_caja'
-                    className='bg-neutral-500 p-2 rounded-lg w-full text-center placeholder:text-neutral-50 text-neutral-50 outline-none'
-                    value={species_caja}
-                    style={{ display: 'none' }}
-                    placeholder='species'
-                    name='species_caja'
-                    autoComplete='off'
-                    onChange={e => setspecies_caja(e.target.value)}
-                >
-                </input>
-
-                <p id='errorspecies' style={{ display: 'none' }} className='text-red-700 bg-zinc-300	border-2 rounded-md'></p>
-
-                <input
-                    type="text"
-                    id='description_caja'
-                    className='bg-neutral-500 p-2 rounded-lg w-full text-center placeholder:text-neutral-50 text-neutral-50 outline-none'
-                    value={description_caja}
-                    style={{ display: 'none' }}
-                    placeholder='description'
-                    name='description_caja'
-                    autoComplete='off'
-                    onChange={e => setdescription_caja(e.target.value)}
-                >
-                </input>
-
-                <p id='errordescription' style={{ display: 'none' }} className='text-red-700 bg-zinc-300	border-2 rounded-md'></p>
-
-                <input
-                    type="text"
-                    id='soil_caja'
-                    className='bg-neutral-500 p-2 rounded-lg w-full text-center placeholder:text-neutral-50 text-neutral-50 outline-none'
-                    value={soil_caja}
-                    style={{ display: 'none' }}
-                    placeholder='Soil'
-                    name='soil_caja'
-                    autoComplete='off'
-                    onChange={e => setsoil_caja(e.target.value)}
-                >
-                </input>
-
-                <p id='errorsoil' style={{ display: 'none' }} className='text-red-700 bg-zinc-300	border-2 rounded-md'></p>
-
-                <input
-                    type="text"
-                    id='temperature_caja'
-                    className='bg-neutral-500 p-2 rounded-lg w-full text-center placeholder:text-neutral-50 text-neutral-50 outline-none'
-                    value={temperature_caja}
-                    style={{ display: 'none' }}
-                    placeholder='Temperature'
-                    name='temperature_caja'
-                    autoComplete='off'
-                    onChange={e => settemperature_caja(e.target.value)}
-                >
-                </input>
-
-                <p id='errortemperature' style={{ display: 'none' }} className='text-red-700 bg-zinc-300	border-2 rounded-md'></p>
-
-                <input
-                    type="text"
-                    id='sun_caja'
-                    className='bg-neutral-500 p-2 rounded-lg w-full text-center placeholder:text-neutral-50 text-neutral-50 outline-none'
-                    value={sun_caja}
-                    style={{ display: 'none' }}
-                    placeholder='Sun'
-                    name='sun_caja'
-                    autoComplete='off'
-                    onChange={e => setsun_caja(e.target.value)}
-                >
-                </input>
-
-                <p id='errorsun' style={{ display: 'none' }} className='text-red-700 bg-zinc-300	border-2 rounded-md'></p>
-
-                <input
-                    type="text"
-                    id='water_caja'
-                    className='bg-neutral-500 p-2 rounded-lg w-full text-center placeholder:text-neutral-50 text-neutral-50 outline-none'
-                    value={water_caja}
-                    style={{ display: 'none' }}
-                    placeholder='Water'
-                    name='water_caja'
-                    autoComplete='off'
-                    onChange={e => setwater_caja(e.target.value)}
-                >
-                </input>
-
-                <p id='errorwater' style={{ display: 'none' }} className='text-red-700 bg-zinc-300	border-2 rounded-md'></p>
-
-                <input
-                    type="text"
-                    id='multiplication_caja'
-                    className='bg-neutral-500 p-2 rounded-lg w-full text-center placeholder:text-neutral-50 text-neutral-50 outline-none'
-                    value={multiplication_caja}
-                    style={{ display: 'none' }}
-                    placeholder='Multiplication'
-                    name='multiplication_caja'
-                    autoComplete='off'
-                    onChange={e => setmultiplication_caja(e.target.value)}
-                >
-                </input>
-
-                <p id='errormultiplication' style={{ display: 'none' }} className='text-red-700 bg-zinc-300	border-2 rounded-md'></p>
-
-                <input
-                    type="file"
-                    id='foto'
-                    className='bg-neutral-500 p-2 rounded-lg w-full text-center placeholder:text-neutral-50 text-neutral-50 outline-none'
-                    accept="image/*"
-                    style={{ display: 'none' }}
-                    placeholder='foto'
-                    name='foto'
-                    autoComplete='off'
-                >
-                </input>
-
-                <p id='errorNombreNuevo' style={{ display: 'none' }} className='text-red-700 bg-zinc-300	border-2 rounded-md'></p>
-
-            </form>
 
         </div>
     )
