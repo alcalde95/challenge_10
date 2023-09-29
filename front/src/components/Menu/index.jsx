ee99306daddf99303f4a52f046a35332fcd42bce
Jorge Alcalde <alcalde.ap95@gmail.com>
2023-09-30 00:20:38 +0200

    crada tabla,edit y delete.están sin terminar, funcionales pero con algunas cosa q revisar

--- front/src/components/Menu/index.jsx
+++ front/src/components/Menu/index.jsx
@@ -6,6 +6,14 @@ import { useNavigate } from 'react-router-dom'
 import { useEffect } from 'react'
 import { comprobarEmail, comprobarPassword, mostrar, ocultar } from '../comprobaciones.jsx'
 
+const plants = [
+    { id: 1,name: "Cy Ganderton", species: "Blue", description: "Blue", soil: "Blue", temperature: "Blue", sun: "Blue", water: "Blue", multiplication: "Blue", image_url: <img className='w-12' src={logo} /> },
+    { id:2 ,name: "Cy Ganderton", species: "Red", description: "Blue", soil: "Blue", temperature: "Blue", sun: "Blue", water: "Blue", multiplication: "Blue", image_url: <img className='w-12' src={logo} /> },
+    { id:3 ,name: "Cy Ganderton", species: "Green", description: "Blue", soil: "Blue", temperature: "Blue", sun: "Blue", water: "Blue", multiplication: "Blue", image_url: <img className='w-12' src={logo} /> }
+]
+
+
+
 const Menu = () => {
 
     const [nombre_caja, setnombre_caja] = useState('')
@@ -17,6 +25,105 @@ const Menu = () => {
     const [water_caja, setwater_caja] = useState('')
     const [multiplication_caja, setmultiplication_caja] = useState('')
 
+
+    const Row = (props) => {
+        const { name, species, description, soil, temperature, sun, water, multiplication, image_url, edit,ddelete } = props
+        return (<tr>
+            <td>{name}</td>
+            <td>{species}</td>
+            <td>{description}</td>
+            <td>{soil}</td>
+            <td>{temperature}</td>
+            <td>{sun}</td>
+            <td>{water}</td>
+            <td>{multiplication}</td>
+            <td>{image_url}</td>
+            <td>{edit}</td>
+            <td>{ddelete}</td>
+        </tr>)
+    }
+
+
+
+    const Table = (props) => {
+        const { data } = props
+        return (
+            <table className="table-auto border-separate border-spacing-x-2.5 border-spacing-y-2.5 border border-slate-500 bg-amber-950">
+                <thead>
+                    <tr className='border border-slate-900 bg-slate-600'>
+                        <th >Name</th>
+                        <th >species </th>
+                        <th >description </th>
+                        <th >soil  </th>
+                        <th >temperature  </th>
+                        <th >sun  </th>
+                        <th >water  </th>
+                        <th >multiplication  </th>
+                        <th >image_url  </th>
+                        <th >edit</th>
+                        <th >ddelete</th>
+                    </tr>
+                </thead>
+                <tbody className='border border-slate-900 bg-slate-600'>
+                    {data.map(row =>
+                        <>
+                            <Row name={row.name}
+                                species={row.species}
+                                description={row.description}
+                                soil={row.soil}
+                                temperature={row.temperature}
+                                sun={row.sun}
+                                water={row.water}
+                                multiplication={row.multiplication}
+                                image_url={row.image_url}
+                                edit = { <button onClick={() => editDelete(row.name, row.species, row.description, row.soil, row.temperature, row.sun, row.water, row.multiplication, row.image_url,2)}>Edit</button>}
+                                ddelete = { <button onClick={() => editDelete(row.name, row.species, row.description, row.soil, row.temperature, row.sun, row.water, row.multiplication, row.image_url,1)}>ddelete</button>}
+                            />
+                           
+                        </>
+                    )}
+
+                </tbody>
+            </table>
+        )
+    }
+
+    const editDelete = (name, species, description, soil, temperature, sun, water, multiplication, image_url,editOrDelete) => {
+
+        editOrDelete == 1 ? stadeReadonly(true) : stadeReadonly(false)
+
+        document.getElementById('caja_nombre').style.display = 'block'
+        document.getElementById('caja_nombre').value = name
+        setnombre_caja(name)
+
+        //esto preguntar a stephan
+
+        document.getElementById('species_caja').style.display = 'block'
+        document.getElementById('species_caja').value = species
+        setspecies_caja(species)
+
+        document.getElementById('description_caja').style.display = 'block'
+        document.getElementById('description_caja').value = description
+
+        document.getElementById('soil_caja').style.display = 'block'
+        document.getElementById('soil_caja').value = soil
+
+        document.getElementById('temperature_caja').style.display = 'block'
+        document.getElementById('temperature_caja').value = temperature
+
+        document.getElementById('sun_caja').style.display = 'block'
+        document.getElementById('sun_caja').value = sun
+
+        document.getElementById('water_caja').style.display = 'block'
+        document.getElementById('water_caja').value = water
+
+        document.getElementById('multiplication_caja').style.display = 'block'
+        document.getElementById('multiplication_caja').value = multiplication
+
+        document.getElementById('foto').style.display = 'block'
+    }
+
+    
     const mostrarAnadir = () => {
         document.getElementById('caja_nombre').style.display = 'block'
         document.getElementById('species_caja').style.display = 'block'
@@ -30,14 +137,31 @@ const Menu = () => {
 
     }
 
+    const stadeReadonly = (stade) => {
+        document.getElementById('caja_nombre').readOnly = stade
+        document.getElementById('species_caja').readOnly = stade
+        document.getElementById('description_caja').readOnly = stade
+        document.getElementById('soil_caja').readOnly = stade
+        document.getElementById('temperature_caja').readOnly = stade
+        document.getElementById('sun_caja').readOnly = stade
+        document.getElementById('water_caja').readOnly = stade
+        document.getElementById('multiplication_caja').readOnly = stade
+        document.getElementById('foto').readOnly = stade
+    }
+
     const comprobarNombre = () => {
         nombre_caja.length < 3 || nombre_caja.length > 20 ? mostrar('errorNombreCaja', 'El tamaño del nombre debe estar entre 3 y 20 caracteres') : ocultar('errorNombreCaja')
         return 3 >= nombre_caja.length >= 20
     }
 
     const comprobardescription_caja = () => {
-        description_caja.length < 3 || description_caja.length > 20 ? mostrar('errordescription', 'El tamaño de la descripción debe estar entre 25 y 200 caracteres') : ocultar('errordescription')
+        description_caja.length < 25 || description_caja.length > 200 ? mostrar('errordescription', 'El tamaño de la descripción debe estar entre 25 y 200 caracteres') : ocultar('errordescription')
         return 25 >= description_caja.length >= 200
+    }
+
+    const comprobarspecies_caja = () => {
+        species_caja.length < 3 || species_caja.length > 20 ? mostrar('errorspecies', 'El tamaño del species debe estar entre 3 y 20 caracteres') : ocultar('errorspecies')
+        return 3 >= species_caja.length >= 25
     }
 
     const comprobarsoil_caja = () => {
@@ -60,7 +184,7 @@ const Menu = () => {
         multiplication_caja.length < 3 || multiplication_caja.length > 20 ? mostrar('errormultiplication', 'El tamaño del multiplication debe estar entre 3 y 20 caracteres') : ocultar('errormultiplication')
         return 3 >= multiplication_caja.length >= 20
     }
-   
+
 
     return (
         <div className='flex flex-col justify-center items-center h-[100vh] bg-gray-500 text-neutral-50'>
@@ -219,63 +343,8 @@ const Menu = () => {
 
             </form>
 
-            <table className="table-auto border-separate border-spacing-x-2.5 border-spacing-y-2.5 border border-slate-500 bg-amber-950">
 
-                <thead className=''>
-                    <tr>
-                        <th></th>
-                        <th className='border border-slate-900 bg-slate-600'>Name</th>
-                        <th className='border border-slate-900 bg-slate-600'>species </th>
-                        <th className='border border-slate-900 bg-slate-600'>description </th>
-                        <th className='border border-slate-900 bg-slate-600'>soil  </th>
-                        <th className='border border-slate-900 bg-slate-600'>temperature  </th>
-                        <th className='border border-slate-900 bg-slate-600'>sun  </th>
-                        <th className='border border-slate-900 bg-slate-600'>water  </th>
-                        <th className='border border-slate-900 bg-slate-600'>multiplication  </th>
-                        <th className='border border-slate-900 bg-slate-600'>image_url  </th>
-                    </tr>
-                </thead>
-                <tbody  >
-                    <tr >
-                        <th className='border border-slate-600 bg-slate-700'>1</th>
-                        <td className='border border-slate-600 bg-slate-700'>Cy Ganderton</td>
-                        <td className='border border-slate-600 bg-slate-700'>Quality Control Specialist</td>
-                        <td className='border border-slate-600 bg-slate-700'>Blue</td>
-                        <td className='border border-slate-600 bg-slate-700'>Blue</td>
-                        <td className='border border-slate-600 bg-slate-700'>Blue</td>
-                        <td className='border border-slate-600 bg-slate-700'>Blue</td>
-                        <td className='border border-slate-600 bg-slate-700'>Blue</td>
-                        <td className='border border-slate-600 bg-slate-700'>Blue</td>
-                        <td className='border border-slate-600 bg-slate-700'> <img className='w-12' src={logo} /></td>
-                    </tr>
-                    <tr className='border border-slate-600 bg-slate-700'>
-                        <th>2</th>
-                        <td>Hart Hagerty</td>
-                        <td>Desktop Support Technician</td>
-                        <td>Purple</td>
-                        <td>Blue</td>
-                        <td>Blue</td>
-                        <td>Blue</td>
-                        <td>Blue</td>
-                        <td>Blue</td>
-                        <td>Blue</td>
-                    </tr>
-                    <tr className='border border-slate-600 bg-slate-700'>
-                        <th>3</th>
-                        <td>Brice Swyre</td>
-                        <td>Tax Accountant</td>
-                        <td>Red</td>
-                        <td>Blue</td>
-                        <td>Blue</td>
-                        <td>Blue</td>
-                        <td>Blue</td>
-                        <td>Blue</td>
-                        <td>Blue</td>
-                    </tr>
-                </tbody>
-            </table>
-
-
+            <Table data={plants}></Table>
         </div>
     )
 
